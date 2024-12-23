2899a89668dbfd397aaa3ca78863ea203534119a
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-12-23 18:32:40 +0530

    In Backend, There is no issue

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.js";
 import todoRoutes from "./routes/todo.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,12 @@ connectDB();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/todo", todoRoutes);
 
